11dfadf4cefe5d29fce9bf7c8b2c05e288f73650
matt <matthewcopeland1@gmail.com>
2012-04-24 00:22:24 -0700

    first push to repo

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(function() {
+  
+  $('.one').click(function(){
+    $('.box').addClass('show-two');
+  });
+
+  $('.two').click(function(){
+    $('.box').addClass('show-three');
+    $('.box').removeClass('show-two');
+  })
+
+  $('.three').click(function(){
+    $('.box').addClass('show-four');
+    $('.box').removeClass('show-three');
+  });
+
+  $('.four').click(function(){
+    $('.box').removeClass('show-four');
+  });
+
+});
